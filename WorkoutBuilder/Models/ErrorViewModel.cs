f72fb4e0731829efde60a1dc80b9fa1bca6284f3
Mike Devlin <mike@intrepiware.com>
2023-11-03 05:43:16 -0500

    Initial commit

---
+++ WorkoutBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkoutBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
