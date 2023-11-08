e40775d0b8a1b8b40a7518e83d5f6a7033c1a1b4
Mike Devlin <mike@intrepiware.com>
2023-11-08 09:43:07 -0600

    Add DbContext and Focus objects

---
+++ WorkoutBuilder.Data/Focus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutBuilder.Data
+{
+    public class Focus
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
