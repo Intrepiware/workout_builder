0b65354cb701194ed23a66de69fa011d8202056c
Mike Devlin <mike@intrepiware.com>
2023-11-17 13:10:37 -0600

    Fix main navigation menu

--- WorkoutBuilder/ClientApp/config/webpack.config.js
+++ WorkoutBuilder/ClientApp/config/webpack.config.js
@@ -1,32 +1,33 @@
 ﻿const path = require("path");
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.jsx']
-    },
-    entry: {
-        "home-index": "./src/home-index.tsx"
-    },
-    output: {
-        path: path.resolve(__dirname, "../../wwwroot/js/pages"),
-        filename: "[name].js"
-    },
-    module: {
-        rules: [
-            {
-                use: {
-                    loader: "babel-loader"
-                },
-                test: /\.js$/,
-                exclude: /node_modules/ //excludes node_modules folder from being transpiled by babel. We do this because it's a waste of resources to do so.
-            },            
-            {
-                use: {
-                    loader: "ts-loader"
-                },
-                test: /\.tsx?$/,
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
+  },
+  entry: {
+    "home-index": "./src/home-index.tsx",
+    site: "./src/site.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "../../wwwroot/js/pages"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: "babel-loader",
+        },
+        test: /\.js$/,
+        exclude: /node_modules/, //excludes node_modules folder from being transpiled by babel. We do this because it's a waste of resources to do so.
+      },
+      {
+        use: {
+          loader: "ts-loader",
+        },
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
