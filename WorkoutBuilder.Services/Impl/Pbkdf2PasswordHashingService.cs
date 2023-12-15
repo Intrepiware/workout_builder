73cb6a8bbb4bca9d5779fb2b314279fb71b2860e
Mike Devlin <mike@intrepiware.com>
2023-12-15 06:00:58 -0600

    Switch out password hashers, and fully implement

--- WorkoutBuilder.Services/Impl/Pbkdf2PasswordHashingService.cs
+++ WorkoutBuilder.Services/Impl/Pbkdf2PasswordHashingService.cs
@@ -1,28 +1,11 @@
-﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-using System.Security.Cryptography;
+﻿using Microsoft.AspNet.Identity;
 
 namespace WorkoutBuilder.Services.Impl
 {
     public class Pbkdf2PasswordHashingService
     {
-        public string Hash(string password)
-        {
-            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
-            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+        public string Hash(string password) => new PasswordHasher().HashPassword(password);
 
-            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
-            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password!,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 100000,
-                numBytesRequested: 256 / 8));
-            return hashed;
-        }
-
-        public bool Verify(string password, string hash)
-        {
-            return false;
-        }
+        public bool Verify(string password, string hash) => new PasswordHasher().VerifyHashedPassword(hash, password) != PasswordVerificationResult.Failed;
     }
 }
