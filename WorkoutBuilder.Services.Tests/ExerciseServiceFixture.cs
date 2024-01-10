2091ffac3309024af7e4285d45884ef485dbd6f2
Mike Devlin <mike@intrepiware.com>
2024-01-10 06:48:33 -0600

    More unit tests

--- WorkoutBuilder.Services.Tests/ExerciseServiceFixture.cs
+++ WorkoutBuilder.Services.Tests/ExerciseServiceFixture.cs
@@ -121,5 +121,67 @@ namespace WorkoutBuilder.Services.Tests
                 StringAssert.Contains("equipment", ex.Message);
             }
         }
+
+        [TestFixture]
+        public class When_Searching
+        {
+            ExerciseService ExerciseService = null!;
+
+            [SetUp]
+            public void Setup()
+            {
+                var exercises = new[]
+                {
+                    new Exercise { Id = 1, Name = "find by name", FocusId = (byte)Models.Focus.Cardio, Equipment = "xxx"},
+                    new Exercise { Id = 2, Name = "find by name 2", FocusId = (byte) Models.Focus.Abs, Equipment = "xxx"},
+                    new Exercise { Id = 3, Name = "xxx", FocusId = (byte)Models.Focus.Strength, Equipment = "find by equipment"},
+                    new Exercise { Id = 4, Name = "xxx", FocusId = (byte)Models.Focus.Abs, Equipment = "find by equipment"}
+                };
+                var exerciseRepository = new TestRepo<Exercise>(exercises);
+                var userContext = A.Fake<IUserContext>();
+                A.CallTo(() => userContext.CanReadAllExercises()).Returns(true);
+                ExerciseService = new ExerciseService { ExerciseRepository = exerciseRepository, UserContext = userContext };
+            }
+
+            [Test]
+            public void Should_Reject_Unauthorized()
+            {
+                var userContext = A.Fake<IUserContext>();
+                A.CallTo(() => userContext.CanReadAllExercises()).Returns(false);
+
+                var exerciseService = new ExerciseService { UserContext = userContext };
+
+                Assert.Throws<SecurityException>(() => exerciseService.Search(0, 0));
+            }
+
+            [Test]
+            public void Should_Find_By_Name()
+            {
+                var results = ExerciseService.Search(10, 0, "find by name");
+                Assert.That(results.Count, Is.EqualTo(2));
+                Assert.That(results.First().Id, Is.EqualTo(1));
+                Assert.That(results.Last().Id, Is.EqualTo(2));
+            }
+
+            [Test]
+            public void Should_Find_By_Focus()
+            {
+                var results = ExerciseService.Search(10, 0, focus: "Abs");
+                Assert.That(results.Count, Is.EqualTo(2));
+                Assert.That(results.First().Id, Is.EqualTo(2));
+                Assert.That(results.Last().Id, Is.EqualTo(4));
+
+            }
+
+            [Test]
+            public void Should_Find_By_Equipment()
+            {
+                var results = ExerciseService.Search(10, 0, equipment: "find by equipment");
+                Assert.That(results.Count, Is.EqualTo(2));
+                Assert.That(results.First().Id, Is.EqualTo(3));
+                Assert.That(results.Last().Id, Is.EqualTo(4));
+
+            }
+        }
     }
 }
