ac22775b3563baa63262584be441f21763c6997f
Mike Devlin <mike@intrepiware.com>
2024-01-11 20:13:43 -0600

    Add ping endpoint

---
+++ WorkoutBuilder/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutBuilder.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Ping()
+        {
+            return NoContent();
+        }
+    }
+}
