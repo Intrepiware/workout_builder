726284d9cec52dcc28ada4862de3a4e6f7faef54
Mike Devlin <mike@intrepiware.com>
2024-01-17 06:10:29 -0600

    Store encryption keys in the database

---
+++ WorkoutBuilder.Data/Migrations/WorkoutBuilderKeys/20240117120902_AddDataProtectionKeys.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutBuilder.Data.Migrations.WorkoutBuilderKeys
+{
+    /// <inheritdoc />
+    public partial class AddDataProtectionKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "workouts");
+
+            migrationBuilder.CreateTable(
+                name: "DataProtectionKeys",
+                schema: "workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FriendlyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Xml = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataProtectionKeys",
+                schema: "workouts");
+        }
+    }
+}
---
+++ WorkoutBuilder.Data/WorkoutBuilderKeysContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkoutBuilder.Data
+{
+    public class WorkoutBuilderKeysContext : DbContext, IDataProtectionKeyContext
+    {
+        // A recommended constructor overload when using EF Core 
+        // with dependency injection.
+        public WorkoutBuilderKeysContext(DbContextOptions<WorkoutBuilderKeysContext> options)
+            : base(options) { }
+
+        // This maps to the table that stores keys.
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configure default schema
+            modelBuilder.HasDefaultSchema("workouts");
+        }
+    }
+}
